701b9569a60d2d13e9e2a9e51bb111915a3b9217
boztopuz <boztopuz@gmail.com>
2023-06-04 23:44:52 +0300

    Initial Commit

---
+++ taxCalculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  taxCalculator
+//
+//  Created by Burak Öztopuz on 4.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ taxCalculator/taxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  taxCalculatorApp.swift
+//  taxCalculator
+//
+//  Created by Burak Öztopuz on 4.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct taxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
