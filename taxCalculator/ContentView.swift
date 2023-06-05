4a3d469cd267a73a87db20a4de8c197f6d688f3b
boztopuz <boztopuz@gmail.com>
2023-06-05 14:49:08 +0300

    "Creted basic UI Design, added calculator variables"

--- taxCalculator/ContentView.swift
+++ taxCalculator/ContentView.swift
@@ -8,14 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @State private var araçBedeli = 0.0
+    @State private var vergisizBedel = 0
+    @State private var ötvBedeli = 0
+    @State private var motorHacmi = "1600cc"
+//    @State private var kdvBedeli = 0.0
+    
+    let ötvYüzdeListesi = [45.0,130.0,220.0]
+    let araçMotorHacmi = ["1600cc'e kadar", "1601-2000cc arası", "2000cc ve üzeri"]
+    
+    var hamBedel: Double {
+        var vergisiz = Double(vergisizBedel)
+        let bedel = Double(araçBedeli)
+        
+        if motorHacmi == araçMotorHacmi[0]{
+            let finalResult = (bedel * 100) / (100 + ötvYüzdeListesi[0])
+            vergisiz = finalResult
+            return vergisiz
+        }else if motorHacmi == araçMotorHacmi[1]{
+            let finalResult = (bedel * 100) / (100 + ötvYüzdeListesi[1])
+            vergisiz = finalResult
+            return vergisiz
         }
-        .padding()
+        
+        let finalResult = (bedel * 100) / (100 + ötvYüzdeListesi[2])
+        vergisiz = finalResult
+        return vergisiz
+    }
+    
+    var ötvTutatı : Double {
+        let vergiMiktarı = araçBedeli - hamBedel
+        return vergiMiktarı
+    }
+    
+    var kdvBedeli: Double {
+        let kdv = ötvTutatı + (araçBedeli * 18 / 100)
+        return kdv
+    }
+    
+    var kdvTutarı : Double {
+        let vergiMiktarı = araçBedeli - kdvBedeli
+        return vergiMiktarı
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Araç Bedeli", value: $araçBedeli, format: .currency(code: Locale.current.currencyCode ?? "TRY"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Motor Hacmi", selection: $motorHacmi) {
+                        ForEach(araçMotorHacmi, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                }header: {
+                    Text("Araç bedeli")
+                }
+                
+                Section{
+                    Text(hamBedel, format: .currency(code: Locale.current.currencyCode ?? "TRY"))
+                }header: {
+                    Text("ÖTV hariç araç Fiyatı")
+                }
+                Section{
+                    Text(ötvTutatı, format: .currency(code: Locale.current.currencyCode ?? "TRY"))
+                }header: {
+                    Text("Ötv tutarı")
+                }
+                Section{
+                    Text(kdvBedeli, format: .currency(code: Locale.current.currencyCode ?? "TRY"))
+                }header: {
+                    Text("KDV Hariç Araç Fiyatı")
+                }
+                Section{
+                    Text(kdvTutarı, format: .currency(code: Locale.current.currencyCode ?? "TRY"))
+                }header: {
+                    Text("KDV tutarı")
+                }
+            }
+            .navigationTitle(Text("Vergi Hesapla"))
+        }
     }
 }
 
